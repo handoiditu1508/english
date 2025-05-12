537de6954cf5946bcca6c0f57384926550e087e5
Danh <handoiditu1508@gmail.com>
2025-05-12 23:27:01 +0700

    update simple present

--- src/modules/Main/pages/SimplePresentPage.tsx
+++ src/modules/Main/pages/SimplePresentPage.tsx
@@ -1,11 +1,13 @@
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
-import { Box, Divider, Typography } from "@mui/material";
+import WarningIcon from "@mui/icons-material/Warning";
+import { Alert, AlertTitle, Box, Divider, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import ContentBox from "../components/ContentBox";
 import HighlightSpan from "../components/HighlightSpan";
 import SubTitle from "../components/SubTitle";
+import SubTitle2 from "../components/SubTitle2";
 import Title from "../components/Title";
 
 function SimplePresentPage() {
@@ -24,6 +26,8 @@ function SimplePresentPage() {
       </ul>
 
       <SubTitle>{t("Structure")}</SubTitle>
+
+      <SubTitle2>To be</SubTitle2>
       <ContentBox>
         <Box sx={{
           display: "flex",
@@ -42,7 +46,73 @@ function SimplePresentPage() {
           <Typography sx={{
             flex: 1,
           }}>
-            S + <HighlightSpan>V<sub>s/es</sub></HighlightSpan> + ...
+            S + <HighlightSpan>is/am/are</HighlightSpan> + ...
+          </Typography>
+        </Box>
+        <Divider variant="fullWidth" sx={{ marginY: 2 }} />
+        <Box sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 2,
+          paddingX: 2,
+        }}>
+          <RemoveCircleOutlineIcon
+            color="error"
+            fontSize="large"
+            sx={{
+              flexGrow: 0,
+              flexShrink: 0,
+            }}
+          />
+          <Typography sx={{
+            flex: 1,
+          }}>
+            S + <HighlightSpan>is/am/are</HighlightSpan> + not + ...
+          </Typography>
+        </Box>
+        <Divider variant="fullWidth" sx={{ marginY: 2 }} />
+        <Box sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 2,
+          paddingX: 2,
+        }}>
+          <HelpOutlineIcon
+            color="info"
+            fontSize="large"
+            sx={{
+              flexGrow: 0,
+              flexShrink: 0,
+            }}
+          />
+          <Typography sx={{
+            flex: 1,
+          }}>
+            <HighlightSpan>is/am/are</HighlightSpan> + S + ... ?
+          </Typography>
+        </Box>
+      </ContentBox>
+
+      <SubTitle2>Verb</SubTitle2>
+      <ContentBox>
+        <Box sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 2,
+          paddingX: 2,
+        }}>
+          <AddCircleOutlineIcon
+            color="success"
+            fontSize="large"
+            sx={{
+              flexGrow: 0,
+              flexShrink: 0,
+            }}
+          />
+          <Typography sx={{
+            flex: 1,
+          }}>
+            S + <HighlightSpan>V<sub>1/s/es</sub></HighlightSpan> + ...
           </Typography>
         </Box>
         <Divider variant="fullWidth" sx={{ marginY: 2 }} />
@@ -89,8 +159,35 @@ function SimplePresentPage() {
         </Box>
       </ContentBox>
 
+      <SubTitle>When to use s or es</SubTitle>
+      <p>Use <HighlightSpan>es</HighlightSpan> when verb end with these characters:</p>
+      <ul>
+        <li><HighlightSpan>o</HighlightSpan>: {"do => does, go => goes"}</li>
+        <li><HighlightSpan>s</HighlightSpan>: {"miss => misses"}</li>
+        <li><HighlightSpan>ch</HighlightSpan>: {"watch => watches"}</li>
+        <li><HighlightSpan>x</HighlightSpan>: {"fix => fixes"}</li>
+        <li><HighlightSpan>sh</HighlightSpan>: {"wash => washes"}</li>
+      </ul>
+      <p>Use <HighlightSpan>s</HighlightSpan> when verb does not end with those characters above.</p>
+
       <SubTitle>{t("Signs to recognize")}</SubTitle>
-      <Typography>ago, last, yesterday, in (a year in the past), ...</Typography>
+      <p>Adverbs of frequency:</p>
+      <ul>
+        <li>always = often</li>
+        <li>usually = frequently</li>
+        <li>generally = normally = regularly</li>
+        <li>sometimes = occasionally</li>
+        <li>rarely = seldom</li>
+        <li>never</li>
+      </ul>
+      <Alert severity="info" variant="outlined" icon={<WarningIcon fontSize="inherit" />} sx={{ marginTop: 2 }}>
+        <AlertTitle>Notice</AlertTitle>
+        Sometime ≠ Sometimes
+        <ul>
+          <li>Sometime = one time, unspecified point in time</li>
+          <li>Sometimes = some times, occasionally</li>
+        </ul>
+      </Alert>
 
       <SubTitle>{t("Examples")}</SubTitle>
       <ol>
@@ -113,6 +210,20 @@ function SimplePresentPage() {
           <ul>
             <li>I <HighlightSpan>do</HighlightSpan> not have English class on Tuesday.</li>
             <li><HighlightSpan>Do</HighlightSpan> I have English class on Tuesday?</li>
+          </ul>
+        </li>
+        <li>
+          I <HighlightSpan>go</HighlightSpan> to school every day.
+          <ul>
+            <li>I <HighlightSpan>do</HighlightSpan> not go to school every day.</li>
+            <li><HighlightSpan>Do</HighlightSpan> I go to school every day?</li>
+          </ul>
+        </li>
+        <li>
+          Hanoi <HighlightSpan>is</HighlightSpan> the capital of Vietnam.
+          <ul>
+            <li>Hanoi <HighlightSpan>is</HighlightSpan> not the capital of Vietnam.</li>
+            <li><HighlightSpan>Is</HighlightSpan> Hanoi the capital of Vietnam?</li>
           </ul>
         </li>
       </ol>
