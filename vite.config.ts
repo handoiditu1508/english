aef597811c5e99a47bf93af0422d083c2c706eab
Danh <handoiditu1508@gmail.com>
2025-06-25 00:27:08 +0700

    config for deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/english",
   plugins: [react()],
   envDir: "environments",
   resolve: {
